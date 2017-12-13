fc7ffe5c9e0cb7cc60e90b1f2bb51afef45c77c3
Rui Fortes <ruisantosfortes@gmail.com>
2017-12-13 19:16:37 +0000

    Use index instead of shit to avoid changing input

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = function flatmap (fn) {
-  var queue = []
+  var queue = [], i
   return function (read) {
     return function again (abort, cb) {
       if(abort)        return read(abort, cb)
-      if(queue.length) return cb(null, queue.shift())
-
+      if(i) return cb(null, queue[--i])
       read(null, function (err, data) {
         if(err) return cb(err)
         queue = fn(data)
+        i = queue.length
         again(null, cb) //cb or read again if queue is empty.
       })
     }

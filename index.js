f32e7a6094583f2ed77b5eeed7e9f70cfe8c36b3
Rui Fortes <ruisantosfortes@gmail.com>
2017-12-13 23:54:35 +0000

    fix iteration order

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ module.exports = function flatmap (fn) {
   var queue = [], i
   return function (read) {
     return function again (abort, cb) {
-      if(abort)        return read(abort, cb)
-      if(i) return cb(null, queue[--i])
+      if(abort)            return read(abort, cb)
+      if(i < queue.length) return cb(null, queue[i++])
       read(null, function (err, data) {
         if(err) return cb(err)
         queue = fn(data)
-        i = queue.length
+        i = 0
         again(null, cb) //cb or read again if queue is empty.
       })
     }
